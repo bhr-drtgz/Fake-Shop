1b2c0e0f997ad83d553d2924dbdc99b0b81bf2d5
bhr-drtgz <akrepwkral@gmail.com>
2022-12-08 22:00:53 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Companent/Navbar';
+import Home from './Companent/Home';
+
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
